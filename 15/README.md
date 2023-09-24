54f6ecc17d00bbfbc8384c94161856825551809a
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-09-24 00:47:50 -0300

    resolved problem 15

---
+++ 15/README.md
@@ -0,0 +1,8 @@
+<h1>Lattice Paths</h1>
+<h2>Problem 15</h2>
+
+<p>Starting in the top left corner of a <i>2 x 2<i> grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.</p>
+<p><strong>How many such routes are there through a 20 x 20 grid?</strong></p>
+
+<h2>Resultados</h2>
+<p>Tempo de execução: 0.002s</p>
---
+++ 15/resolucao.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+uint64_t factorial(int n, int less);
+uint64_t sequence(int n);
+
+int main()
+{
+    printf("%" PRIu64 "\n", sequence(40));
+    return 0;
+}
+
+uint64_t factorial(int n, int less)
+{
+    uint64_t factorial = 1;
+    if (n < 1) return 1;
+    for (int i = less + 1; i <= n; i ++) factorial *= i;
+    return factorial;
+}
+
+uint64_t sequence(int n)
+{
+    /*
+     * f(1) = 2
+     * f(n) = f(n-1) * (2n)! / [2(n-1)]! / n * n
+     *
+     */
+
+    int i = 2;
+    uint64_t fn = 2;
+    while (i <= n / 2)
+    {
+        fn = fn * factorial(2 * i, 2 * (i - 1)) / (i * i);
+        i++;
+    }
+    return fn;
+}
