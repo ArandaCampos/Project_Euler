11f7dca913abf6b92568a83dca1e2ea2b7d6b1ba
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-09-24 01:20:40 -0300

    resolved problem 15

--- 15/resolucao.c
+++ 15/resolucao.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
 
-uint64_t factorial(int n, int less);
-uint64_t sequence(int grid);
+long int sequence(int grid);
 
 int main()
 {
-    printf("%" PRIu64 "\n", sequence(20));
+    printf("%ld\n", sequence(20));
     return 0;
 }
 
-uint64_t factorial(int n, int less)
-{
-    uint64_t factorial = 1;
-    if (n < 1) return 1;
-    for (int i = less + 1; i <= n; i ++) factorial *= i;
-    return factorial;
-}
-
-uint64_t sequence(int grid)
+long int sequence(int grid)
 {
     /*
-     * f(1) = 2
-     * f(n) = 1 * 2 * ... * 2(n - 1) * ... * 2n / (1 * 2 * ... * n-1 * n ) * (1 * 2 * ... * n-1 * n)
-     * f(n) = f(n-1) * 2n! / 2(n-1)! / n * n
+     * f(1)     = 2
+     *
+     * f(n)     = 1 * 2 * ... * 2n                    / (1 * 2 * ... * n)        * (1 * 2 * ... * n)
+     * f(n + 1) = 1 * 2 * ... * 2n * (2n+1) * (2n+2) / (1 * 2 * ... * n * n+1 ) * (1 * 2 * ... * n * n+1)
+     * ---------------------------------------------------------------------------------------------------
+     * f(n + 1) = f(n)             * (2n+1) * (2n+2) / (                  n+1) * (                   n+1)
+     *
+     * f(n) = f(n-1) * 2n * (2n-1) / n * n
      *
      */
 
     int n = 2;
-    uint64_t fn = 2;
+    long int fn = 2;
     while (n <= grid)
     {
-        fn = fn * factorial(2 * n, 2 * (n - 1)) / (n * n);
+        fn = fn * 2 * n * (2 * n - 1) / (n * n);
         n++;
     }
     return fn;
