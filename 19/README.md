c5884a8ba85f46f83e74c4f618b72b9046e1f086
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-12-10 21:11:59 -0300

    resolved problem 19

---
+++ 19/README.md
@@ -0,0 +1,15 @@
+<h1>Counting Sundays</h1>
+<h2>Problem 19</h2>
+<p>You are given the following information, but you may prefer to do some research for yourself.</p>
+
+<ul>
+<li>1 Jan 1900 was a Monday.</li>
+<li>Thirty days has September, April, June and November. All the rest have thirty-one. Saving February alone, Which has twenty-eight, rain or shine. And on leap years, twenty-nine.
+<li>A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.</li>
+</ul>
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+</p>
+<h2>Resultados</h2>
+<p>Tempo de execução: 0.002s</p>
+<p>Complexidade de tempo: O(12n)</p>
+<p>Complexidade de memória: O(1)</p>
---
+++ 19/resolucao.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int isLeapYear(int);
+int fellOnTheFirstDay(int, int, int);
+
+int main ()
+{
+    int q = 0;
+    for (int i = 1901; i <= 2000; i++){
+        q += isLeapYear(i);
+    }
+    printf("Anos = %d\n", q);
+    printf("%d\n", fellOnTheFirstDay(1901, 2000, 0));
+    return 0;
+}
+
+int isLeapYear(int year){
+    // A v B = A v (C ^ D)
+    // ¬A ^ ¬B = ¬A ^ (¬C v ¬D)
+    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)) return 1;
+    return 0;
+}
+
+int fellOnTheFirstDay(int yearStart, int yearFinish, int dayOfWeek)
+{
+    int n = 0;
+    int year = 1900;
+    int day = 1; // monday
+    int months[] = {
+        31 % 7,  // Jan
+        28 % 7,  // Fev
+        31 % 7,  // Mar
+        30 % 7,  // Abr
+        31 % 7,  // Mai
+        30 % 7,  // Jun
+        31 % 7,  // Jul
+        31 % 7,  // Ago
+        30 % 7,  // Set
+        31 % 7,  // Out
+        30 % 7,  // Nov
+        31 % 7   // Dez
+    };
+
+    while (year < yearStart){
+        if ( isLeapYear(year) ) day += 2 % 7;
+        else day += 1;
+        year++;
+    }
+
+    day = day % 7;
+
+    while (year <= yearFinish){
+        for (int i = 0; i < 12; i++){
+            if (day == dayOfWeek)
+            {
+                n++;
+            }
+            day += months[i];
+
+            if ( isLeapYear(year) && i == 1 ){
+                day += 1;
+            }
+            day = day % 7;
+        }
+        year++;
+    }
+
+    return n;
+}
